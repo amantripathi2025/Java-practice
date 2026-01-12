cc8b0348beb1bfad01ae8426b4dacc7ef08f9d85
Aman Pati Tripathi <25LBCS1236@culkomail.in>
2026-01-12 19:46:20 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Even Odd identifier: ");
+        System.out.println("Enter the number: ");
+        int num = input.nextInt();
+        
+        if(num%2 == 0){
+            System.out.println("Number is Even");
+        }
+
+        else{
+            System.out.println("Number is Odd");
+        }
+
+        input.close();
+    }
+}
