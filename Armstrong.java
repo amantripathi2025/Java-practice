d49be9ef4ba03010b77d19db399f9c5e977ee2f2
Aman Pati Tripathi <25LBCS1236@culkomail.in>
2026-01-16 21:41:06 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,28 @@
+// import java.util.Scanner;
+// class Armstrong {
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         System.out.println("Please enter the number: ");
+//         int num = input.nextInt();
+//         boolean isArmstrong = isArmstrong(num);
+//         if(isArmstrong){
+//             System.out.println("Your number is Armstrong");
+//         } else{
+//             System.out.println("Your number is not Armstrong");
+//         }
+//         input.close();
+//     }
+//       public static boolean isArmstrong(){
+//         int digits = noOfDigits(num);
+//         return false;
+//       }
+
+//       public static int noOfDigits(int num){
+//         int digits = 0;
+//         while(num > 0){
+//             digits ++;
+//             num /= 10;
+//         }
+//         return digits;
+//       }
+// }
