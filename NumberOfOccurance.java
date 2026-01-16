e2595b3489387aa8efe13d00ab6825f266cd9e26
Aman Pati Tripathi <25LBCS1236@culkomail.in>
2026-01-16 21:40:33 +0530

    Add files via upload

---
+++ NumberOfOccurance.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class NumberOfOccurance {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Array Occurances");
+        System.out.println("Please enter the number of elements: ");
+        int size = input.nextInt();
+         int[] nums = new int[size];
+        System.out.println("Enter the number: ");
+
+    }
+}
