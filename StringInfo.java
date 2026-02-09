de8ddf75052627beef3a734d3a8be0096fc51da2
Aman Pati Tripathi <25LBCS1236@culkomail.in>
2026-02-09 23:12:30 +0530

    Add files via upload

---
+++ StringInfo.java
@@ -0,0 +1,13 @@
+package example.com;
+
+public class StringInfo {
+        public static void main(String[] args) {
+            String str = "JavaProgramming";
+
+            System.out.println("Length: " + str.length());
+            System.out.println("First character: " + str.charAt(0));
+            System.out.println("Last character: " + str.charAt(str.length() - 1));
+        }
+    }
+
+
