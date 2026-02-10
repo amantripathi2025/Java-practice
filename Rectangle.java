b86ef24130343e3ce3f36db8f3479b0b2cedaa34
Aman Pati Tripathi <25LBCS1236@culkomail.in>
2026-02-10 23:14:45 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,11 @@
+package in.kgcoding.challange77.Geometry;
+
+public class Rectangle {
+    public int length;
+    public int breadth;
+
+    public Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+}
