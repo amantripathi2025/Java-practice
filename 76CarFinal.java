a2a5f9b84adb5766cfda3608a8cbcd1ba34a5ed5
Aman Pati Tripathi <25LBCS1236@culkomail.in>
2026-02-11 18:39:52 +0530

    Add files via upload

---
+++ 76CarFinal.java
@@ -0,0 +1,16 @@
+package in.kgcoding;
+
+ class CarFinal {
+     final int noOfWheels;
+
+     final String model;
+
+     final String engineInLitres;
+
+     public CarFinal(int noOfWheels, String model, String engineInLitres) {
+         this.noOfWheels = noOfWheels;
+         this.model = model;
+         this.engineInLitres = engineInLitres;
+         
+     }
+ }
