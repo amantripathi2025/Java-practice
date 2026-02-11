e6c6e3b1bae1c13bd3cb9010c98cacccfc7ae366
Aman Pati Tripathi <25LBCS1236@culkomail.in>
2026-02-11 18:38:13 +0530

    Add files via upload

---
+++ 71 StringManipulation.java
@@ -0,0 +1,12 @@
+package in.kgcoding;
+
+ class StringManipulation {
+     public static void main(String[]args) {
+         String firstName = "Aman";
+         String lastName = "Tripathi";
+         String fullName = firstName.concat( " ").concat(lastName);
+         //System.out.println(fullName);
+         System.out.println(fullName.toUpperCase());
+
+     }
+}
