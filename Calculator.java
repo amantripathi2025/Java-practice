6bfcfc93fcc05436a5f2be517d3f46c729520eaa
Aman Pati Tripathi <25LBCS1236@culkomail.in>
2026-06-03 21:31:54 +0530

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,17 +1,35 @@
-package in.kgcoding.challange77.Utils;
 
-import in.kgcoding.challange77.Geometry.Circle;
-import in.kgcoding.challange77.Geometry.Rectangle;
 
 public class Calculator {
-    public static void main(String[]args){
-        Circle cir = new Circle(5.5);
-        Rectangle rect = new Rectangle(5,4);
 
-        double cirArea = Math.PI * Math.pow(cir.radius,2);
-        double rectArea = rect.length * rect.breadth;
-
-        System.out.printf("Area of circle is %f\nArea of rectangle is %f" ,cirArea, rectArea);
+    public static double add(double a, double b) {
+        return a + b;
     }
 
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static double divide(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+
+    public static double percentage(double value, double percent) {
+        return (value * percent) / 100;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("10 + 5 = " + add(10, 5));
+        System.out.println("10 - 5 = " + subtract(10, 5));
+        System.out.println("10 * 5 = " + multiply(10, 5));
+        System.out.println("10 / 5 = " + divide(10, 5));
+        System.out.println("20% of 100 = " + percentage(100, 20));
+    }
 }
